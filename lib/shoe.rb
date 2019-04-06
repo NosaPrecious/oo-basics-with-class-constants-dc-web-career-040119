9741230f73df4019ca71cdfd3b27a3856b1b3682
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-04-06 20:11:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,26 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS= []
   def initialize(brand)
     @brand = brand
+  end
+  
+  def brand=(brand_name)
+    @brand= brand_name
+    BRANDS.any?{
+      |brand_type|
+    brand_type
+    }
+    BRANDS << brand
+    
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+  binding.pry
 end
